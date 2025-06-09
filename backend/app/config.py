1d61525314927b1479551f8ed828d37dbdddb24b
Yashaswi Mohanty <josh.mohanty@gmail.com>
2025-06-09 16:58:53 +0530

    Add Jansunani API keys

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,17 +1,19 @@
 import os
 from pydantic_settings import BaseSettings 
 from pathlib import Path
-from loguru import logger
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     ENV: str = os.getenv("ENV", "dev")
-    DEBUG: bool = ENV != "prod"
-    
+    DEBUG: bool = os.getenv("DEBUG", "True").lower() in ("true", "1", "yes")
+    JANSUNANI_API_USERNAME: str = os.getenv("JANSUNANI_API_USERNAME")
+    JANSUNANI_API_PASSWORD: str = os.getenv("JANSUNANI_API_PASSWORD")
+
     class Config:
         env_file = ROOT_DIR / ".env"
 
 settings = Settings()
+
 
 

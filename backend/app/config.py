6587525c334b4cbd66fbc6d1001886ea9a7a80e4
Yashaswi Mohanty <josh.mohanty@gmail.com>
2025-06-10 12:22:41 +0530

    Blacken code

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,20 +1,21 @@
 import os
-from pydantic_settings import BaseSettings 
+from pydantic_settings import BaseSettings
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
+
 class Settings(BaseSettings):
     ENV: str = os.getenv("ENV", "dev")
     DEBUG: bool = os.getenv("DEBUG", "True").lower() in ("true", "1", "yes")
-    JANSUNANI_API_BASE_URL: str = os.getenv("JANSUNANI_API_BASE_URL", "https://janasunani.odisha.gov.in/api/DataServices")
+    JANSUNANI_API_BASE_URL: str = os.getenv(
+        "JANSUNANI_API_BASE_URL", "https://janasunani.odisha.gov.in/api/DataServices"
+    )
     JANSUNANI_API_USERNAME: str = os.getenv("JANSUNANI_API_USERNAME")
     JANSUNANI_API_PASSWORD: str = os.getenv("JANSUNANI_API_PASSWORD")
 
     class Config:
         env_file = ROOT_DIR / ".env"
 
+
 settings = Settings()
-
-
-
